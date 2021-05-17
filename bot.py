f827c0c030bf16c2007e355988c19d99c1a4f4fa
elias <elias@DESKTOP-VB073CU.localdomain>
2021-05-17 10:07:50 -0300

    Declaración de variables genericas

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import time
 
 def api_conection():
     #Create CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+    CONSUMER_KEY = '----------------------------'
+    CONSUMER_SECRET = '--------------------------'
+    ACCESS_TOKEN = '--------------------------------------------------------------'
+    ACCESS_TOKEN_SECRET = '---------------------------------------------------------'
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
